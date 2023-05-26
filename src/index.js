c616ef174e337bdbd0e24b3943745653c4792d87
StarkLSH <seungheon1029@gmail.com>
2023-05-26 10:53:17 +0900

    feat: redux-thunk counter에 적용

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import rootReducer from './modules';
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 //import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger'
+import ReduxThunk from 'redux-thunk'
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger))
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
