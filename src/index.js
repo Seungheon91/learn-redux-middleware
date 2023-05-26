3872d455c9d2835ca49e9aca5336c2ed2816955f
StarkLSH <seungheon1029@gmail.com>
2023-05-26 15:59:24 +0900

    feat: rootsaga 추가

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 //import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger'
 import ReduxThunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk))
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk, sagaMiddleware))
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+sagaMiddleware.run(rootSaga)
 
 root.render(
   <Provider store={store}>
