8a8fd3745956e291de3b85dfb0dd6d01e62cfbe6
StarkLSH <seungheon1029@gmail.com>
2023-05-26 15:04:55 +0900

    refactor: sample reducer, thunk 리팩토링

--- src/containers/SampleContainer.js
+++ src/containers/SampleContainer.js
@@ -5,8 +5,15 @@ import { getPost, getUsers } from "../modules/sample";
 
 const SampleContainer = ({ getPost, getUsers, post, users, loadingPost, loadingUsers }) => {
   useEffect(() => {
-    getPost(1);
-    getUsers(1);
+    const fn = async () => {
+      try {
+        await getPost(1);
+        await getUsers(1);
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fn();
   }, [getPost, getUsers])
   return (
     <Sample
@@ -19,11 +26,11 @@ const SampleContainer = ({ getPost, getUsers, post, users, loadingPost, loadingU
 }
 
 export default connect(
-  ({ sample }) => ({
+  ({ sample, loading }) => ({
     post: sample.post,
     users: sample.users,
-    loadingPost: sample.loading.GET_POST,
-    loadingUsers: sample.loading.GET_USERS
+    loadingPost: loading['sample/GET_POST'],
+    loadingUsers: loading['sample/GET_USERS']
   }),
   {
     getPost,
