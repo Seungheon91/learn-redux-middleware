71a8c42c9afe0a33f6184bf0bef535f3d127812f
StarkLSH <seungheon1029@gmail.com>
2023-05-30 09:45:40 +0900

    refactor: redux-saga 코드 리팩토링

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions'
-import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import { delay, put, select, takeEvery, takeLatest, throttle } from 'redux-saga/effects'
 
 const INCREASE = 'counter/INCREASE'
 const DECREASE = 'counter/DECREASE'
@@ -15,6 +15,8 @@ export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined)
 function* increaseSaga() {
   yield delay(1000) // 1초 기다림
   yield put(increase()) // 특정 액션을 디스패치
+  const number = yield select(state => state.counter) // state는 스토어 상태
+  console.log(`현재 값은 ${number} 입니다.`)
 }
 
 
@@ -25,7 +27,8 @@ function* decreaseSaga() {
 
 export function* counterSaga() {
   // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리해줌
-  yield takeEvery(INCREASE_ASYNC, increaseSaga)
+  //yield takeEvery(INCREASE_ASYNC, increaseSaga)
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga)
   // takeLatest는 기존에 진행 중이던 작업이 있다면 취소 처리하고 가장 마지막으로 실행된 작업만 수행합니다.
   yield takeLatest(DECREASE_ASYNC, decreaseSaga)
 }
